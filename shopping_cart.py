b85da4efd11b7f96d9f9bf89f54a79922690fedb
chathurikav20 <132036400+chathurikav20@users.noreply.github.com>
2023-05-03 18:34:09 +0530

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,12 +1,23 @@
+# shopping_cart.py
+
 class ShoppingCart:
+    
     def __init__(self):
-        self.items = []
-
-    def add_item(self, item):
-        pass
-
+        self.items = {}
+        
+    def add_item(self, item, price):
+        if item not in self.items:
+            self.items[item] = price
+            
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+            
     def view_cart(self):
-        pass
-
-    def calculate_total_price(self):
-        pass
+        output = ''
+        for item, price in self.items.items():
+            output += f'{item}: {price:.2f}\n'
+        return output
+    
+    def calculate_total(self):
+        return sum(self.items.values())
