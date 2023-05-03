3ca1f9e8e8f7fc17c114ab367675a21f59bbd0e0
chathurikav20 <132036400+chathurikav20@users.noreply.github.com>
2023-05-03 17:55:35 +0530

    Create shopping_cart.py

---
+++ shopping_cart.py
@@ -0,0 +1,12 @@
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        pass
+
+    def view_cart(self):
+        pass
+
+    def calculate_total_price(self):
+        pass
